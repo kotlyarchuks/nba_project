b6c25112852c3e85185dd223a4c5b5c8cf328678
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2018-10-08 18:07:48 +0200

    Boxscore for any game, reddit posts on index, styling and refactoring

---
+++ games/utils.py
@@ -0,0 +1,113 @@
+from django.utils import timezone
+import requests
+import urllib.request
+import json
+
+
+def api_request(url):
+    req_headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Host': 'stats.nba.com',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    }
+    req = requests.get(url, headers=req_headers)
+    if req.status_code == 200:
+        return req.json()
+    else:
+        raise Exception('Something went wrong with request to NBA!')
+
+
+def today_games_list():
+    today = timezone.now() + timezone.timedelta(days=-1)
+    today = today.strftime("%Y-%m-%d")
+    url = f"https://stats.nba.com/stats/scoreboardv2?DayOffset=0&GameDate={today}&LeagueID=00"
+    response = api_request(url)
+    games_list = [game[2] for game in response['resultSets'][0]['rowSet']]
+    data = []
+    for game in games_list:
+        url = f"https://stats.nba.com/stats/boxscoresummaryv2?GameID={game}"
+        response = api_request(url)
+        row = response['resultSets'][5]['rowSet']
+        if row[0][-1] > row[1][-1]:
+            winner = 'home'
+        else:
+            winner = 'away'
+        data.append({
+            'id': game,
+            'home_team': row[0][5] + " " + row[0][6],
+            'away_team': row[1][5] + " " + row[1][6],
+            'home_team_score': row[0][-1],
+            'away_team_score': row[1][-1],
+            'winner': winner
+        })
+    return data
+
+
+def get_json(url):
+    with urllib.request.urlopen(url) as url_res:
+        raw_data = json.loads(url_res.read().decode())
+    return raw_data
+
+
+def get_boxscore(game_id):
+    raw_data = get_json(
+        f"https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2018/scores/gamedetail/00{game_id}_gamedetail.json")['g']
+
+    data = {
+        'home_team': raw_data['hls']['tc'] + " " + raw_data['hls']['tn'],
+        'home_team_short': raw_data['hls']['ta'],
+        'away_team': raw_data['vls']['tc'] + " " + raw_data['vls']['tn'],
+        'away_team_short': raw_data['vls']['ta'],
+        'score': f"{raw_data['hls']['s']} - {raw_data['vls']['s']}",
+        'home_fg': {
+            'fga': raw_data['hls']['tstsg']['fga'],
+            'fgm': raw_data['hls']['tstsg']['fgm'],
+            'fgp': str(round((raw_data['hls']['tstsg']['fgm'] / raw_data['hls']['tstsg']['fga'])*100, 1)) + "%"
+        },
+        'away_fg': {
+            'fga': raw_data['vls']['tstsg']['fga'],
+            'fgm': raw_data['vls']['tstsg']['fgm'],
+            'fgp': str(round((raw_data['vls']['tstsg']['fgm'] / raw_data['vls']['tstsg']['fga'])*100, 1)) + "%"
+        },
+        'home_3p': {
+            '3pa': raw_data['hls']['tstsg']['tpa'],
+            '3pm': raw_data['hls']['tstsg']['tpm'],
+            '3pp': str(round((raw_data['hls']['tstsg']['tpm'] / raw_data['hls']['tstsg']['tpa'])*100, 1)) + "%"
+        },
+        'away_3p': {
+            '3pa': raw_data['vls']['tstsg']['tpa'],
+            '3pm': raw_data['vls']['tstsg']['tpm'],
+            '3pp': str(round((raw_data['vls']['tstsg']['tpm'] / raw_data['vls']['tstsg']['tpa'])*100, 1)) + "%"
+        },
+        'home_players': raw_data['hls']['pstsg'],
+        'away_players': raw_data['vls']['pstsg'],
+    }
+    return data
+
+
+def get_reddit_posts():
+    req_headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    }
+    url = "https://www.reddit.com/r/nba/hot.json?limit=20"
+    req = requests.get(url, headers=req_headers)
+    raw_data = req.json()['data']['children']
+    return [
+        {
+            'title': post['data']['title'],
+            'text':post['data']['selftext'],
+            'score':post['data']['score'],
+            'url':post['data']['url'],
+            'comments':post['data']['num_comments'],
+            'link':post['data']['permalink'],
+        }
+        for post in raw_data]
--- games/views.py
+++ games/views.py
@@ -1,71 +1,18 @@
 from django.shortcuts import render
-import requests
-from django.utils import timezone
+from .utils import api_request, today_games_list, get_json, get_boxscore, get_reddit_posts
 
-import urllib.request
-import json
 
 # https://stats.nba.com/stats/scoreboardv2?DayOffset=0&GameDate=2018-10-07&LeagueID=00
 
 
-def nba_request(url):
-    req_headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive',
-        'Host': 'stats.nba.com',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-    }
-    req = requests.get(url, headers=req_headers)
-    if req.status_code == 200:
-        return req.json()
-    else:
-        raise Exception('Something went wrong with request to NBA!')
-
-
 def index(request):
-    today = timezone.now() + timezone.timedelta(days=-1)
-    today = today.strftime("%Y-%m-%d")
-    url = f"https://stats.nba.com/stats/scoreboardv2?DayOffset=0&GameDate={today}&LeagueID=00"
-    response = nba_request(url)
-    games_list = [game[2] for game in response['resultSets'][0]['rowSet']]
-    data = []
-    for game in games_list:
-        url = f"https://stats.nba.com/stats/boxscoresummaryv2?GameID={game}"
-        response = nba_request(url)
-        row = response['resultSets'][5]['rowSet']
-        if row[0][-1] > row[1][-1]:
-            winner = 'home'
-        else:
-            winner = 'away'
-        data.append({
-            'id': game,
-            'home_team': row[0][5] + " " + row[0][6],
-            'away_team': row[1][5] + " " + row[1][6],
-            'home_team_score': row[0][-1],
-            'away_team_score': row[1][-1],
-            'winner': winner
-        })
+    data = today_games_list()
+    posts = get_reddit_posts()
 
-    return render(request, 'games/index.html', {'data': data})
+    return render(request, 'games/index.html', {'data': data, 'posts': posts})
 
 
 def detail(request, game_id):
-    # url = f"https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2018/scores/gamedetail/00{game_id}_gamedetail.json"
-    # req_headers = {
-    #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    #     'Accept-Encoding': 'gzip, deflate',
-    #     'Accept-Language': 'en-US,en;q=0.8',
-    #     'Connection': 'keep-alive',
-    #     'Host': 'stats.nba.com',
-    #     'Upgrade-Insecure-Requests': '1',
-    #     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-    # }
-    # req = requests.get(url, headers=req_headers)
-    url = f"https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2018/scores/gamedetail/00{game_id}_gamedetail.json"
-    with urllib.request.urlopen(url) as url_res:
-        data = json.loads(url_res.read().decode())
+    data = get_boxscore(game_id)
 
     return render(request, 'games/detail.html', {'data': data})
