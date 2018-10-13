337966780f47f2638fb5f4d466dc6a3c5591434f
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2018-10-13 19:32:58 +0200

    Debugging

--- games/utils.py
+++ games/utils.py
@@ -14,7 +14,7 @@ def api_request(url):
         # 'Connection': 'keep-alive',
         'Host': 'stats.nba.com',
         # 'Upgrade-Insecure-Requests': '1',
-        # 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
     }
     req = requests.get(url, headers=req_headers)
     if req.status_code == 200:
